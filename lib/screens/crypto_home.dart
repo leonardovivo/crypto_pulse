3561da4eb19cc2651ac8aa531a964f40665ea1e4
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-01-05 20:36:09 -0300

    Tela com fundo e mensagem de escolha

--- lib/screens/crypto_home.dart
+++ lib/screens/crypto_home.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_pulse/widgets/color.dart';
 import 'package:flutter/material.dart';
 
 class CryptoPulseHome extends StatelessWidget {
@@ -7,9 +8,53 @@ class CryptoPulseHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Container(
-        color: const Color.fromARGB(255, 41, 13, 71),
-      )),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/fundo_home.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 100,
+                left: 120,
+                child: Text(
+                  'Choose your',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 140,
+                left: 100,
+                child: Text(
+                  'Crypto',
+                  style: TextStyle(
+                    color: CyberYellow.cyberpunkYellow,
+                    fontFamily: 'Rovelink',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
+                    shadows: [
+                      Shadow(
+                        color: Colors.blueAccent,
+                        blurRadius: 10,
+                        offset: Offset(6, 5),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
