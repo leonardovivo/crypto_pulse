3f03034f09be0a60831aac6a44d44554df546839
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-01-08 20:05:17 -0300

    Botões adicionados

--- lib/screens/crypto_home.dart
+++ lib/screens/crypto_home.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_pulse/widgets/clickable_image.dart';
 import 'package:crypto_pulse/widgets/color.dart';
 import 'package:flutter/material.dart';
 
@@ -31,6 +32,7 @@ class CryptoPulseHome extends StatelessWidget {
                   ),
                 ),
               ),
+
               Positioned(
                 top: 140,
                 left: 100,
@@ -46,6 +48,63 @@ class CryptoPulseHome extends StatelessWidget {
                         color: Colors.blueAccent,
                         blurRadius: 10,
                         offset: Offset(6, 5),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                top: 300,
+                child: Center(
+                  child: GridView.count(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    padding: const EdgeInsets.all(20),
+                    children: [
+                      ClickableImage(
+                        imagePath: 'assets/images/bitcoin.png',
+                        onTap: () => print('Bitcoin Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/ethereum.png',
+                        onTap: () => print('Ethereum Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/xrp.png',
+                        onTap: () => print('Ripple Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/dogecoin.png',
+                        onTap: () => print('Dogecoin Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/tether.png',
+                        onTap: () => print('Litecoin Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/cardano.png',
+                        onTap: () => print('Cardano Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/usd.png',
+                        onTap: () => print('Polkadot Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/solana.png',
+                        onTap: () => print('Solana Clicked!'),
+                      ),
+
+                      ClickableImage(
+                        imagePath: 'assets/images/binance.png',
+                        onTap: () => print('Binance Clicked!'),
                       ),
                     ],
                   ),
