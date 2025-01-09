38570fae6ff94ff45cfa00764458362dece81f82
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-01-09 17:54:48 -0300

    Rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:crypto_pulse/screens/binance_screen.dart';
+import 'package:crypto_pulse/screens/bitcoin_screen.dart';
+import 'package:crypto_pulse/screens/cardano_screen.dart';
 import 'package:crypto_pulse/screens/crypto_home.dart';
+import 'package:crypto_pulse/screens/dogecoin_screen.dart';
+import 'package:crypto_pulse/screens/ethereum_screen.dart';
+import 'package:crypto_pulse/screens/solana_screen.dart';
+import 'package:crypto_pulse/screens/tether_screen.dart';
+import 'package:crypto_pulse/screens/usd_screen.dart';
+import 'package:crypto_pulse/screens/xrp_screen.dart';
 import 'package:crypto_pulse/widgets/color.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +26,17 @@ class CryptoPulse extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const CryptoPulseHome(),
+      routes: {
+          '/bitcoin': (context) => const BitcoinScreen(),
+          '/ethereum': (context) => const EthereumScreen(),
+          '/xrp': (context) => const XrpScreen(),
+          '/dogecoin': (context) => const DogecoinScreen(),
+          '/tether': (context) => const TetherScreen(),
+          '/cardano': (context) => const CardanoScreen(),
+          '/usd': (context) => const UsdScreen(),
+          '/solana': (context) => const SolanaScreen(),
+          '/binance': (context) => const BinanceScreen(),
+        },
     );
   }
 }
