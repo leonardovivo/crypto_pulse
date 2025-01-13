2491e888a5baca513e7783c6ae04b3d2053a45c7
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-01-13 20:25:14 -0300

    Scroll infinito

--- lib/screens/bitcoin_screen.dart
+++ lib/screens/bitcoin_screen.dart
@@ -38,7 +38,6 @@ class _BitcoinScreenState extends State<BitcoinScreen> {
       child: Scaffold(
         body: Stack(
           children: [
-            // Imagem de fundo
             Container(
               height: double.infinity,
               width: double.infinity,
@@ -49,91 +48,100 @@ class _BitcoinScreenState extends State<BitcoinScreen> {
                 ),
               ),
             ),
-            // AppBar transparente
-            Positioned(
-              top: 50,
-              left: 123,
-              child: Text(
-                'Bitcoin',
-                style: TextStyle(
-                  color: CyberYellow.cyberpunkYellow,
-                  fontFamily: 'Rovelink',
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  shadows: [
-                    Shadow(
-                      color: Colors.blueAccent,
-                      blurRadius: 10,
-                      offset: Offset(6, 5),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // Conteúdo principal
-            cryptoData == null
-                ? Center(
-                    child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation(Colors.greenAccent),
-                    ),
-                  )
-                : Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ListView(
-                      children: [
-                        SizedBox(height: 180),
-                        DetailItem(title: 'Name', value: cryptoData!['name']),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Symbol', value: cryptoData!['symbol']),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Rank', value: '${cryptoData!['rank']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Price (USD)',
-                            value: '\$${cryptoData!['price_usd']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: '24h Change',
-                            value: '${cryptoData!['percent_change_24h']}%'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: '1h Change',
-                            value: '${cryptoData!['percent_change_1h']}%'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: '7d Change',
-                            value: '${cryptoData!['percent_change_7d']}%'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Market Cap',
-                            value: '\$${cryptoData!['market_cap_usd']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Volume (24h)',
-                            value: '\$${cryptoData!['volume24']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Circulating Supply',
-                            value: '${cryptoData!['csupply']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Price (BTC)',
-                            value: '\$${cryptoData!['price_btc']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Total Supply',
-                            value: '\$${cryptoData!['tsupply']}'),
-                        SizedBox(height: 100),
-                        DetailItem(
-                            title: 'Maximum Supply',
-                            value: '\$${cryptoData!['msupply']}'),
-                        SizedBox(height: 100),
-                      ],
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50, left: 10),
+                    child: Text(
+                      'Bitcoin',
+                      style: TextStyle(
+                        color: CyberYellow.cyberpunkYellow,
+                        fontFamily: 'Rovelink',
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            color: Colors.blueAccent,
+                            blurRadius: 10,
+                            offset: Offset(6, 5),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
+                  cryptoData == null
+                      ? Center(
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation(Colors.greenAccent),
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 70),
+                              DetailItem(
+                                  title: 'Name', value: cryptoData!['name']),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Symbol',
+                                  value: cryptoData!['symbol']),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Rank',
+                                  value: '${cryptoData!['rank']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Price (USD)',
+                                  value: '\$${cryptoData!['price_usd']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: '24h Change',
+                                  value:
+                                      '${cryptoData!['percent_change_24h']}%'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: '1h Change',
+                                  value:
+                                      '${cryptoData!['percent_change_1h']}%'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: '7d Change',
+                                  value:
+                                      '${cryptoData!['percent_change_7d']}%'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Market Cap',
+                                  value: '\$${cryptoData!['market_cap_usd']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Volume (24h)',
+                                  value: '\$${cryptoData!['volume24']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Circulating Supply',
+                                  value: '${cryptoData!['csupply']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Price (BTC)',
+                                  value: '\$${cryptoData!['price_btc']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Total Supply',
+                                  value: '\$${cryptoData!['tsupply']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Maximum Supply',
+                                  value: '\$${cryptoData!['msupply']}'),
+                              SizedBox(height: 100),
+                            ],
+                          ),
+                        ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
