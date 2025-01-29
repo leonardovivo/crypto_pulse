b302b4947a3250bfa64432028a9fc9f9b81c26c7
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-01-29 04:25:14 -0300

    Row para Column

--- lib/widgets/detail_item.dart
+++ lib/widgets/detail_item.dart
@@ -15,35 +15,37 @@ class DetailItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              color: CyberYellow.cyberpunkYellow,
-              fontFamily: 'Voltec',
-              fontSize: 35,
-              fontWeight: FontWeight.bold,
-              shadows: [
-                Shadow(
-                  color: Colors.blueAccent,
-                  blurRadius: 10,
-                  offset: Offset(1, 6),
-                ),
-              ],
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: CyberYellow.cyberpunkYellow,
+                fontFamily: 'Voltec',
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Colors.blueAccent,
+                    blurRadius: 10,
+                    offset: Offset(1, 6),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Text(
-            value,
-            style: TextStyle(
-              color: Colors.greenAccent,
-              fontFamily: 'Voltec',
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            Text(
+              value,
+              style: TextStyle(
+                color: Colors.greenAccent,
+                fontFamily: 'Voltec',
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
