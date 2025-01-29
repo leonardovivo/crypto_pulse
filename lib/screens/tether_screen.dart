0034392d4880a1955c7b50530530b326fc7dad12
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-01-29 04:27:24 -0300

    Design padrão das telas de moedas aplicado

--- lib/screens/tether_screen.dart
+++ lib/screens/tether_screen.dart
@@ -1,4 +1,7 @@
+import 'package:crypto_pulse/widgets/color.dart';
+import 'package:crypto_pulse/widgets/detail_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -17,7 +20,8 @@ class _TetherScreenState extends State<TetherScreen> {
   }
 
   Future<void> fetchCryptoData() async {
-    final response = await http.get(Uri.parse('https://api.coinlore.net/api/ticker/?id=518'));
+    final response = await http
+        .get(Uri.parse('https://api.coinlore.net/api/ticker/?id=518'));
     if (response.statusCode == 200) {
       setState(() {
         cryptoData = jsonDecode(response.body)[0];
@@ -29,60 +33,123 @@ class _TetherScreenState extends State<TetherScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tether'),
-      ),
-      body: cryptoData == null
-          ? Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/fundo_moedas.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
                 children: [
-                  _buildDetailItem('Name', cryptoData!['name']),
-                  _buildDetailItem('Symbol', cryptoData!['symbol']),
-                  _buildDetailItem('Rank', '${cryptoData!['rank']}'),
-                  _buildDetailItem(
-                      'Price (USD)', '\$${cryptoData!['price_usd']}'),
-                  _buildDetailItem(
-                      '24h Change', '${cryptoData!['percent_change_24h']}%'),
-                  _buildDetailItem(
-                      '1h Change', '${cryptoData!['percent_change_1h']}%'),
-                  _buildDetailItem(
-                      '7d Change', '${cryptoData!['percent_change_7d']}%'),
-                  _buildDetailItem(
-                      'Market Cap', '\$${cryptoData!['market_cap_usd']}'),
-                  _buildDetailItem(
-                      'Volume (24h)', '\$${cryptoData!['volume24']}'),
-                  _buildDetailItem(
-                      'Circulating Supply', '${cryptoData!['csupply']}'),
-                  _buildDetailItem(
-                      'Price (BTC)', '\$${cryptoData!['price_btc']}'),
-                  _buildDetailItem(
-                      'Total Supply', '\$${cryptoData!['tsupply']}'),
-                  _buildDetailItem(
-                      'Maximum Supply', '\$${cryptoData!['msupply']}'),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50, left: 10),
+                    child: Text(
+                      'Tether',
+                      style: TextStyle(
+                        color: CyberYellow.cyberpunkYellow,
+                        fontFamily: 'Rovelink',
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            color: Colors.blueAccent,
+                            blurRadius: 10,
+                            offset: Offset(6, 5),
+                          ),
+                        ],
+                      ),
+                    )
+                        .animate(
+                          onPlay: (controller) => controller.repeat(),
+                        )
+                        .shimmer(
+                          duration: 2000.ms,
+                          color: CyberYellow.cyberpunkYellow,
+                        ),
+                  ),
+                  cryptoData == null
+                      ? Center(
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation(Colors.greenAccent),
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 70),
+                              DetailItem(
+                                  title: 'Name', value: cryptoData!['name']),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Symbol',
+                                  value: cryptoData!['symbol']),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Rank',
+                                  value: '${cryptoData!['rank']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Price (USD)',
+                                  value: '\$${cryptoData!['price_usd']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: '24h Change',
+                                  value:
+                                      '${cryptoData!['percent_change_24h']}%'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: '1h Change',
+                                  value:
+                                      '${cryptoData!['percent_change_1h']}%'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: '7d Change',
+                                  value:
+                                      '${cryptoData!['percent_change_7d']}%'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Market Cap',
+                                  value: '\$${cryptoData!['market_cap_usd']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Volume (24h)',
+                                  value: '\$${cryptoData!['volume24']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Circulating Supply',
+                                  value: '${cryptoData!['csupply']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Price (BTC)',
+                                  value: '\$${cryptoData!['price_btc']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Total Supply',
+                                  value: '\$${cryptoData!['tsupply']}'),
+                              SizedBox(height: 100),
+                              DetailItem(
+                                  title: 'Maximum Supply',
+                                  value: '\$${cryptoData!['msupply']}'),
+                              SizedBox(height: 100),
+                            ],
+                          ),
+                        ),
                 ],
               ),
             ),
-    );
-  }
-
-  Widget _buildDetailItem(String title, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            value,
-            style: TextStyle(fontSize: 18),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
